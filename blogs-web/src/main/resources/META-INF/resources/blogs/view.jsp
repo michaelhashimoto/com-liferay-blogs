robertoDiaz/liferay-portal

LPS-63334 Change my pending entries interaction

modules/apps/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/view.jsp
@@ -23,6 +23,8 @@ String assetTagName = ParamUtil.getString(request, "tag");
 PortletURL portletURL = renderResponse.createRenderURL();
 
 portletURL.setParameter("mvcRenderCommandName", "/blogs/view");
+
+String mvcRenderCommandName = ParamUtil.getString(request, "mvcRenderCommandName");
 %>
 
 <portlet:actionURL name="/blogs/edit_entry" var="restoreTrashEntriesURL">
@@ -38,7 +40,7 @@ portletURL.setParameter("mvcRenderCommandName", "/blogs/view");
 <%
 int pageDelta = GetterUtil.getInteger(blogsPortletInstanceConfiguration.pageDelta());
 
-SearchContainer searchContainer = new SearchContainer(renderRequest, null, null, SearchContainer.DEFAULT_CUR_PARAM, pageDelta, portletURL, null, null);
+SearchContainer searchContainer = new SearchContainer(renderRequest, null, null, SearchContainer.DEFAULT_CUR_PARAM, pageDelta, currentURLObj, null, null);
 
 searchContainer.setDelta(pageDelta);
 searchContainer.setDeltaConfigurable(false);
@@ -46,12 +48,25 @@ searchContainer.setDeltaConfigurable(false);
 int total = 0;
 List results = null;
 
+int pendingEntriesCount = BlogsEntryServiceUtil.getGroupUserEntriesCount(scopeGroupId, themeDisplay.getUserId(), BlogsPortletUtil.getPendingStatuses());
+
+request.setAttribute("view.jsp-pendingEntriesCount", pendingEntriesCount);
+
 if ((assetCategoryId != 0) || Validator.isNotNull(assetTagName)) {
 	SearchContainerResults<AssetEntry> searchContainerResults = BlogsUtil.getSearchContainerResults(searchContainer);
 
 	searchContainer.setTotal(searchContainerResults.getTotal());
 
 	results = searchContainerResults.getResults();
+}
+else if (mvcRenderCommandName.equals("/blogs/view_my_pending_entries")) {
+	total = pendingEntriesCount;
+
+	searchContainer.setTotal(total);
+
+	results = BlogsEntryServiceUtil.getGroupUserEntries(scopeGroupId, themeDisplay.getUserId(), BlogsPortletUtil.getPendingStatuses(), searchContainer.getStart(), searchContainer.getEnd(), new EntryModifiedDateComparator());
+
+	searchContainer.setResults(results);
 }
 else {
 	int status = WorkflowConstants.STATUS_APPROVED;
@@ -65,5 +80,29 @@ else {
 
 searchContainer.setResults(results);
 %>
+
+<c:if test="<%= pendingEntriesCount > 0 %>">
+	<aui:nav-bar cssClass="collapse-basic-search" markupView="lexicon">
+		<aui:nav cssClass="navbar-nav">
+			<portlet:renderURL var="viewEntriesURL" />
+
+			<aui:nav-item
+				href="<%= viewEntriesURL %>"
+				label="entries"
+				selected='<%= !mvcRenderCommandName.equals("/blogs/view_my_pending_entries") %>'
+			/>
+
+			<portlet:renderURL var="viewMyPendingEntriesURL">
+				<portlet:param name="mvcRenderCommandName" value="/blogs/view_my_pending_entries" />
+			</portlet:renderURL>
+
+			<aui:nav-item
+				href="<%= viewMyPendingEntriesURL %>"
+				label='<%= LanguageUtil.format(resourceBundle, "my-pending-entries-x", pendingEntriesCount, false) %>'
+				selected='<%= mvcRenderCommandName.equals("/blogs/view_my_pending_entries") %>'
+			/>
+		</aui:nav>
+	</aui:nav-bar>
+</c:if>
 
 <%@ include file="/blogs/view_entries.jspf" %>

michaelhashimoto/com-liferay-blogs

blogs-web/src/main/resources/META-INF/resources/blogs/view.jsp
@@ -23,6 +23,8 @@ String assetTagName = ParamUtil.getString(request, "tag");
 PortletURL portletURL = renderResponse.createRenderURL();
 
 portletURL.setParameter("mvcRenderCommandName", "/blogs/view");
+
+String mvcRenderCommandName = ParamUtil.getString(request, "mvcRenderCommandName");
 %>
 
 <portlet:actionURL name="/blogs/edit_entry" var="restoreTrashEntriesURL">
@@ -38,7 +40,7 @@ portletURL.setParameter("mvcRenderCommandName", "/blogs/view");
 <%
 int pageDelta = GetterUtil.getInteger(blogsPortletInstanceConfiguration.pageDelta());
 
-SearchContainer searchContainer = new SearchContainer(renderRequest, null, null, SearchContainer.DEFAULT_CUR_PARAM, pageDelta, portletURL, null, null);
+SearchContainer searchContainer = new SearchContainer(renderRequest, null, null, SearchContainer.DEFAULT_CUR_PARAM, pageDelta, currentURLObj, null, null);
 
 searchContainer.setDelta(pageDelta);
 searchContainer.setDeltaConfigurable(false);
@@ -46,12 +48,25 @@ searchContainer.setDeltaConfigurable(false);
 int total = 0;
 List results = null;
 
+int[] statuses = new int[]{WorkflowConstants.STATUS_DRAFT, WorkflowConstants.STATUS_SCHEDULED};
+
+int notPublishedEntriesCount = BlogsEntryServiceUtil.getGroupUserEntriesCount(scopeGroupId, themeDisplay.getUserId(), statuses);
+
 if ((assetCategoryId != 0) || Validator.isNotNull(assetTagName)) {
 	SearchContainerResults<AssetEntry> searchContainerResults = BlogsUtil.getSearchContainerResults(searchContainer);
 
 	searchContainer.setTotal(searchContainerResults.getTotal());
 
 	results = searchContainerResults.getResults();
+}
+else if (mvcRenderCommandName.equals("/blogs/view_not_published_entries")) {
+	total = notPublishedEntriesCount;
+
+	searchContainer.setTotal(total);
+
+	results = BlogsEntryServiceUtil.getGroupUserEntries(scopeGroupId, themeDisplay.getUserId(), statuses, searchContainer.getStart(), searchContainer.getEnd(), new EntryModifiedDateComparator());
+
+	searchContainer.setResults(results);
 }
 else {
 	int status = WorkflowConstants.STATUS_APPROVED;
@@ -65,5 +80,27 @@ else {
 
 searchContainer.setResults(results);
 %>
+
+<c:if test="<%= notPublishedEntriesCount > 0 %>">
+	<aui:nav-bar cssClass="collapse-basic-search" markupView="lexicon">
+		<aui:nav cssClass="navbar-nav">
+			<aui:nav-item
+				href="<%= portletURL %>"
+				label="published"
+				selected='<%= !mvcRenderCommandName.equals("/blogs/view_not_published_entries") %>'
+			/>
+
+			<portlet:renderURL var="viewNotPublishedEntriesURL">
+				<portlet:param name="mvcRenderCommandName" value="/blogs/view_not_published_entries" />
+			</portlet:renderURL>
+
+			<aui:nav-item
+				href="<%= viewNotPublishedEntriesURL %>"
+				label='<%= LanguageUtil.format(resourceBundle, "not-published-x", notPublishedEntriesCount, false) %>'
+				selected='<%= mvcRenderCommandName.equals("/blogs/view_not_published_entries") %>'
+			/>
+		</aui:nav>
+	</aui:nav-bar>
+</c:if>
 
 <%@ include file="/blogs/view_entries.jspf" %>